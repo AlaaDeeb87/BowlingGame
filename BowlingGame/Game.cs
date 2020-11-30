e49b3f97fb90833a9d7249abbd600f353c6b7585
Alaa Deeb <71987914+AlaaDeeb87@users.noreply.github.com>
2020-11-30 14:13:06 +0100

    Third test

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -1,18 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BowlingGame
 {
     public class Game
     {
-        public int Score { get; set; }
-
-        public void Roll(int pinsHit)
-        {
-            Score += pinsHit;
-        }
+        private List<int> _rolls = new List<int>(21);
+        private int currentRoll = 0;
+        //public Game()
+        //{
+        //    Score = 0;
+        //}
         public Game()
         {
-            Score = 0;
+            for (int i = 0; i < 22; i++)
+            {
+                _rolls.Add(0);
+            }
+        }
+        private int _currentRoll =0;
+        
+        public void Roll(int pinsHit)
+        {
+            ////Score += pinsHit;
+            //_rolls.Add(pinsHit);
+            //currentRoll++;
+            _rolls[_currentRoll++] = pinsHit;
+        }
+      
+        public int Score 
+        {
+            get
+            {
+                int score = 0;
+                int fremeIndex = 0;
+                for (int frame =0; frame <10; frame++)
+                {
+                    if (IsStrike(fremeIndex))
+                    {
+                        score += 10 + StrikeBonus(fremeIndex);
+                        fremeIndex ++;
+                    }
+                    else if (IsSpare(fremeIndex))
+                    {
+                        score += 10 + SpareBonus(fremeIndex);
+                        fremeIndex += 2;
+                    }
+                    else
+                    {
+                        score += NormalFrameBonus(fremeIndex);
+                        fremeIndex += 2;
+                    }
+                    
+                }
+                return score;
+            }
+        }
+
+        private int NormalFrameBonus(int fremeIndex)
+        {
+            return _rolls[fremeIndex ] + _rolls[fremeIndex + 1];
+        }
+
+        private int SpareBonus(int fremeIndex)
+        {
+            return _rolls[fremeIndex + 2];
+        }
+
+        private int StrikeBonus(int fremeIndex)
+        {
+            return _rolls[fremeIndex+1] + _rolls[fremeIndex + 2];
+        }
+
+        private bool IsStrike(int fremeIndex)
+        {
+            return _rolls[fremeIndex] == 10;
+        }
+
+        private bool IsSpare(int fremeIndex)
+        {
+            return _rolls[fremeIndex] + _rolls[fremeIndex + 1] == 10;
         }
     }
 }
--- BowlingGameTest/BowlingGameTest.cs
+++ BowlingGameTest/BowlingGameTest.cs
@@ -15,8 +15,6 @@ namespace BowlingGameTest
             Assert.AreEqual(0, game.Score);
         }
 
-       
-
         [TestMethod]
         public void TestHittingOnePinPerRoll()
         {
@@ -24,6 +22,35 @@ namespace BowlingGameTest
             RollPins(game, 20, 1);
             Assert.AreEqual(20, game.Score);
         }
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            var game = SetupGame();
+            RollSpar(game);
+            game.Roll(3);
+            RollPins(game, 17, 0);
+            Assert.AreEqual(16, game.Score);
+        }
+        [TestMethod]
+        public void TestOneStrike()
+        {
+            var game = SetupGame();
+            RollStrike(game);
+            game.Roll(3);
+            game.Roll(4);
+            RollPins(game, 16, 0);
+            Assert.AreEqual(24, game.Score);
+        }
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            var game = SetupGame();
+            RollPins(game, 12, 10);
+            Assert.AreEqual(300, game.Score);
+        }
+
+
+
         #region Private Mathods
         private Game SetupGame()
         {
@@ -36,6 +63,15 @@ namespace BowlingGameTest
                 game.Roll(pinsHitPerRoll);
             }
 
+        }
+        private void RollSpar(Game game)
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+        private void RollStrike(Game game)
+        {
+            game.Roll(10);
         }
         #endregion
     }
